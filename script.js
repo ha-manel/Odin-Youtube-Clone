0fbef14898367913cbbbda1db35856227e329ed5
Manel <manelhammouche3@gmail.com>
2022-04-26 23:34:20 +0100

    Add responsiveness to website

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const searchString = document.getElementById('search');
 const searchBtn = document.querySelector('#search-btn');
 
 searchBtn.addEventListener('click', () => window.location.href =`https://www.youtube.com/results?search_query= ${searchString.value}`);
+
+const hamburgerBtn = document.querySelector('#hamburger-menu');
+const navLinks = document.querySelector('.links-div');
+
+hamburgerBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+})
